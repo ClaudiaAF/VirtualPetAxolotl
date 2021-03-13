e7e0665d197268409b799bec8731a9f3482b9030
claud <180181@virtualwindow.co.za>
2021-03-13 17:20:58 +0200

    Added timer in App background

--- VirtualPetAxolotl/App.xaml.cs
+++ VirtualPetAxolotl/App.xaml.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using VirtualPetAxolotl.NewFolder1;
 
 namespace VirtualPetAxolotl
 {
     public partial class App : Application
-    {
+    { 
+        private Timekeeper timeKeeper = new Timekeeper();
+
         public App()
         {
             InitializeComponent();
@@ -15,14 +18,25 @@ namespace VirtualPetAxolotl
 
         protected override void OnStart()
         {
+            Console.WriteLine("OnStart");
+
+            Console.WriteLine(timeKeeper.StoredTime);
+            Console.WriteLine(timeKeeper.GetTimeElapsed());
         }
 
         protected override void OnSleep()
         {
+            Console.WriteLine("OnSleep");
+
+            timeKeeper.StoredTime = DateTime.Now;
         }
 
         protected override void OnResume()
         {
+            Console.WriteLine("OnResume");
+
+            Console.WriteLine(timeKeeper.StoredTime);
+            Console.WriteLine(timeKeeper.GetTimeElapsed());
         }
     }
 }
