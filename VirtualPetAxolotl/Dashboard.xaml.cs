47b78b1333a464d9304dc67800b03489a4a60920
claud <180181@virtualwindow.co.za>
2021-03-13 16:40:28 +0200

    Added backend elements to dashboard

--- VirtualPetAxolotl/Dashboard.xaml.cs
+++ VirtualPetAxolotl/Dashboard.xaml.cs
@@ -15,10 +15,59 @@ namespace VirtualPetAxolotl
     public partial class Dashboard : ContentPage
     {
 
+        private  Axolotl axolotl = new Axolotl();
+
+        void updateUI()
+        {
+            int axolotlXp = axolotl.Xp;
+
+            if (axolotlXp < 1)
+            {
+                levelLabel.Text = "Not being cared for";
+                xpLabel.Text = "tap the plant to feed your pet";
+            } else
+            {
+                levelLabel.Text = "Level 1" + Levels.GetLevelFromXp(axolotlXp).ToString();
+                xpLabel.Text = axolotlXp.ToString();
+            }
+
+           
+
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                axolotlImage.Source = "Axolotl_" + axolotl.CurrentAxolotlState + "_" + (Levels.GetLevelFromXp(axolotlXp) + 1).ToString();
+
+                if (axolotl.CurrentAxolotlState == AxolotlState.sick)
+                {
+                    AxolotlDead();
+                }
+            });
+        }
+
         public Dashboard()
         {
             InitializeComponent();
 
+            updateUI();
+
+        }
+
+        void feedAxolotlTapped(System.Object sender, System.EventArgs e)
+        {
+            axolotl.giveFood();
+
+            updateUI();
+        }
+
+        private async void AxolotlDead()
+        {
+            await DisplayAlert("Dead", "Your Axolotl has died", "New Axolotl");
+
+            axolotl.Xp = 0;
+            axolotl.CurrentAxolotlState = AxolotlState.healthy;
+           
+
+            updateUI();
         }
     }
 }
