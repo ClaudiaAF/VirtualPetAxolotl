5236115315c2e8c007bb56269c24fc2376e55048
claud <180181@virtualwindow.co.za>
2021-03-13 16:55:22 +0200

    Added time keeper functions in object

--- VirtualPetAxolotl/Objects1/Timekeeper.cs
+++ VirtualPetAxolotl/Objects1/Timekeeper.cs
@@ -6,5 +6,59 @@ namespace VirtualPetAxolotl.NewFolder1
 {
     public class Timekeeper
     {
+        const string startTimeKey = "startTime";
+        const string storedTimeKey = "storedTime";
+
+        public DateTime StartTime
+        {
+            get
+            {
+                if (App.Current.Properties.ContainsKey(startTimeKey))
+                {
+                    return new DateTime((long)App.Current.Properties[startTimeKey]);
+                }
+                else
+                {
+                    return DateTime.Now;
+                }
+            }
+
+            set
+            {
+                App.Current.Properties[startTimeKey] = value.Ticks;
+            }
+        }
+
+        public DateTime StoredTime
+        {
+            get
+            {
+                if (App.Current.Properties.ContainsKey(storedTimeKey))
+                {
+                    return new DateTime((long)App.Current.Properties[storedTimeKey]);
+                }
+                else
+                {
+                    return DateTime.Now;
+                }
+            }
+
+            set
+            {
+                App.Current.Properties[storedTimeKey] = value.Ticks;
+            }
+
+        }
+
+
+        public Timekeeper()
+        {
+
+        }
+
+        public double GetTimeElapsed()
+        {
+            return (StoredTime - StartTime).TotalSeconds;
+        }
     }
 }
