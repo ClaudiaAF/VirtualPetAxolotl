2aba6e2a6555f8bc58137b61331eb5db47a0f5a4
claud <180181@virtualwindow.co.za>
2021-03-13 16:33:20 +0200

    Add levels functionality

--- VirtualPetAxolotl/Objects1/Level.cs
+++ VirtualPetAxolotl/Objects1/Level.cs
@@ -6,5 +6,24 @@ namespace VirtualPetAxolotl.NewFolder1
 {
     class Levels
     {
+        public static int GetLevelFromXp(int xp)
+        {
+            if (xp == 0)
+            {
+                return 0;
+            }
+            else if (xp < 2000)
+            {
+                return 1;
+            }
+            else if (xp >= 2000)
+            {
+                return 2;
+            } else
+            {
+                return 0;
+            }
+
+        }
     }
 }
