3b53465203e61dfd4b9bf2acfba2d9739c3f11b3
claud <180181@virtualwindow.co.za>
2021-03-13 16:22:15 +0200

    Added Axolotl states

---
+++ VirtualPetAxolotl/Objects1/AxolotlState.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VirtualPetAxolotl.NewFolder1
+{
+    public enum AxolotlState
+    {
+        healthy,
+        nothealthy,
+        sick
+    }
+    class AxolotlStates
+    {
+        public static string GetAxolotlString(AxolotlState axolotlState)
+        {
+            switch (axolotlState)
+            {
+                case AxolotlState.healthy:
+                    return "healthy";
+                case AxolotlState.nothealthy:
+                    return "not healthy";
+                case AxolotlState.sick:
+                    return "sick";
+                default:
+                    return "swimming";
+            }
+        }
+
+        public static AxolotlState GetAxolotlState(string axolotlString)
+        {
+            switch (axolotlString) 
+            {
+                case "healthy":
+                    return AxolotlState.healthy;
+                case "not healthy":
+                    return AxolotlState.nothealthy;
+                case "sick":
+                    return AxolotlState.sick;
+                default:
+                    return AxolotlState.healthy;
+
+
+            }
+
+        }
+    }
+}
--- VirtualPetAxolotl/Objects1/Timekeeper.cs
+++ VirtualPetAxolotl/Objects1/Timekeeper.cs
@@ -4,8 +4,7 @@ using System.Text;
 
 namespace VirtualPetAxolotl.NewFolder1
 {
-    public class Axolotl
+    public class Timekeeper
     {
-        
     }
 }
